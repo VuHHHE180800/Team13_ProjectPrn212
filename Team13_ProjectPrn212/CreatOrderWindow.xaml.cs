aaea5d97ae571cdea4e575cfb623c178016a8314
vuhhhe180800 <vuhhhe180800@fpt.edu.vn>
2024-12-07 23:24:48 +0700

    add create order

---
+++ Team13_ProjectPrn212/CreatOrderWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Team13_ProjectPrn212.Models;
+
+namespace Team13_ProjectPrn212
+{
+    /// <summary>
+    /// Interaction logic for CreatOrderWindow.xaml
+    /// </summary>
+    public partial class CreatOrderWindow : Window
+    {
+        private ObservableCollection<CartItem> cart = new ObservableCollection<CartItem>();
+        private Team13QlbhContext db = new Team13QlbhContext();
+        public CreatOrderWindow()
+        {
+            InitializeComponent();
+            loadBrand();
+            loadCategory();
+            loadForm();
+        }
+
+        private void ProductDataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProductDataGrid.ItemsSource = db.Products.Include(p=>p.Brand).Include(p=>p.Category).Where(p=>p.Quantity>0).ToList();
+        }
+
+        private void btnShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            filterProduct();
+        }
+
+        private void btnLapHoaDon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTaoMoi_Click(object sender, RoutedEventArgs e)
+        {
+            txtTenKhachHang.Text = string.Empty;
+            txtDiaChi.Text = string.Empty;
+            txtSoDienThoai.Text = string.Empty;
+            txtThanhTien.Text = string.Empty;
+            txtMaSanPham.Text = string.Empty;
+            txtTenSanPham.Text = string.Empty;
+            txtDonGia.Text = string.Empty;
+            txtSoLuongMua.Text = "1";
+            cart.Clear();
+
+        }
+
+        private void btnTinhTien_Click(object sender, RoutedEventArgs e)
+        {
+var selectedProduct = ProductDataGrid.SelectedItem as Product;
+
+    if (selectedProduct != null)
+    {
+         // Kiểm tra số lượng nhập từ TextBox
+        int soluong;
+                try
+                {
+                    soluong = int.Parse(txtSoLuongMua.Text);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Vui lòng chỉ nhập số.", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+        if (soluong<=selectedProduct.Quantity )
+        {
+            // Kiểm tra xem sản phẩm đã có trong giỏ hàng chưa
+            var existingItem = cart.FirstOrDefault(item => item.ProductId == selectedProduct.ProductId);
+
+            if (existingItem != null)
+            {
+                // Nếu có, cập nhật số lượng
+                existingItem.Quantity += soluong;
+            }
+            else
+            {
+                // Nếu chưa, thêm sản phẩm mới vào giỏ
+                cart.Add(new CartItem
+                {
+                    ProductId = selectedProduct.ProductId,
+                    ProductName = selectedProduct.ProductName,
+                    Quantity = soluong,
+                    Price = selectedProduct.Price
+                });
+                txtThanhTien.Text = cart.Sum(item =>  item.Price).ToString(); // Câp nhật TextBox txtThanhTien
+            }
+
+            // Làm mới DataGrid hiển thị giỏ hàng
+            CartDataGrid.Items.Refresh();
+        }
+        else
+        {
+            MessageBox.Show("Số lượng không hợp lệ!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+    else
+    {
+        MessageBox.Show("Vui lòng chọn sản phẩm để thêm vào giỏ hàng.", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+    }
+        }
+        private void CartDataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            CartDataGrid.ItemsSource = cart;
+        }
+
+        private void ProductDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var product = ProductDataGrid.SelectedItem as Product;
+            if(product != null)
+            {
+                txtMaSanPham.Text = product.ProductId.ToString();
+                txtTenSanPham.Text = product.ProductName;
+                txtDonGia.Text = product.Price.ToString();
+                txtSoLuongConLai.Text = "Số lượng còn lại: "+product.Quantity.ToString();
+            }
+        }
+        private void loadBrand()
+        {
+            var brands = db.Brands.ToList();
+            cbbNSXTimKiem.ItemsSource = brands;
+            cbbNSXTimKiem.DisplayMemberPath = "BrandName";
+            cbbNSXTimKiem.SelectedValuePath = "BrandId";
+        }
+        private void loadCategory()
+        {
+            var categories = db.Categories.ToList();
+            cbbLoaiTimKiem.ItemsSource = categories;
+            cbbLoaiTimKiem.DisplayMemberPath = "CategoryName";
+            cbbLoaiTimKiem.SelectedValuePath = "CategoryId"; 
+        }
+        private void filterProduct()
+        {
+            List<Product>plist= db.Products.ToList();
+            string productName = txtTenSPTimKiem.Text;
+            if(productName.Length > 0 )
+            {
+                plist = plist.Where(p => p.ProductName.ToLower().Contains(productName)).ToList();
+            }
+
+            int brandId = -1;
+            int categoryId = -1;
+
+            if(cbbLoaiTimKiem.SelectedIndex > -1)
+            {
+                categoryId = (int)cbbLoaiTimKiem.SelectedValue;
+                plist = plist.Where(p => p.CategoryId == categoryId).ToList();
+            }
+            if(cbbNSXTimKiem.SelectedIndex > -1)
+            {
+                brandId = (int)cbbNSXTimKiem.SelectedValue;
+                plist = plist.Where(p => p.BrandId == brandId).ToList();
+            }
+            ProductDataGrid.ItemsSource = plist;
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            txtTenSPTimKiem.Text = string.Empty;
+            cbbLoaiTimKiem.SelectedIndex = -1;
+            cbbNSXTimKiem.SelectedIndex = -1;
+            ProductDataGrid.ItemsSource = db.Products.Include(p => p.Brand).Include(p => p.Category).Where(p => p.Quantity > 0).ToList();
+        }
+        private void loadForm()
+        {
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+            dpNgayLap.Text = today.ToString();
+        }
+
+        
+    }
+}
