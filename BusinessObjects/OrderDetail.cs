d5c436b23d5c5fc1525e19390635c7ba4754851d
Dingirsu2800 <chinhndhe182189@fpt.edu.vn>
2024-12-10 13:59:23 +0700

    Add Business Model

---
+++ BusinessObjects/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects;
+
+public partial class OrderDetail
+{
+    public int OrderId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int TotalPrice { get; set; }
+
+    public int TotalQuantity { get; set; }
+
+    public virtual Order Order { get; set; } = null!;
+
+    public virtual Product Product { get; set; } = null!;
+}
---
+++ BusinessObjects/Report.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects;
+
+public partial class Report
+{
+    public int ReportId { get; set; }
+
+    public string ReportName { get; set; } = null!;
+
+    public DateOnly ReportDate { get; set; }
+
+    public int TotalAmount { get; set; }
+}
